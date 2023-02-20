b50e7c9693f0d146c3ae6835822d41d903885663
nigelritfeld <nigelritfeld@live.nl>
2023-02-20 15:59:11 +0100

    Added new hero

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import { Author } from '../components/Author'
 import { Footer } from '../components/Footer'
 import { FreeChapters } from '../components/FreeChapters'
-import { Hero } from '../components/Hero'
 import { Introduction } from '../components/Introduction'
 import { NavBar } from '../components/NavBar'
 import { Pricing } from '../components/Pricing'
@@ -14,6 +13,7 @@ import { Testimonial } from '../components/Testimonial'
 import { Testimonials } from '../components/Testimonials'
 import avatarImage1 from '../images/avatars/avatar-1.png'
 import avatarImage2 from '../images/avatars/avatar-2.png'
+import FancyHero from "../components/hero/FancyHero";
 
 export default function Home() {
   return (
@@ -27,7 +27,7 @@ export default function Home() {
           content="A book and video course that teaches you how to design your own icons from scratch. "
         />
       </Head>
-      <Hero />
+      <FancyHero />
       <Introduction />
       <NavBar />
       <TableOfContents />
