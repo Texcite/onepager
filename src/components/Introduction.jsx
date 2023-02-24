bcedec98f9d1c598cbedec4f7ecb111a34dd7b3f
Stefan de Gier <stefan_de_gier@outlook.com>
2023-02-24 10:52:44 +0100

    Updated introduction

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -10,7 +10,7 @@ export function Introduction() {
             aria-label="Introduction"
             className="pt-20 pb-16 sm:pb-20 md:pt-36 lg:py-32"
         >
-            <Container className="text-lg tracking-tight text-slate-700 space-y-6">
+            <Container className="text-lg tracking-tight text-slate-700">
 
                 <p className="mt-4">
                     In opdracht van de NPO hebben wij de vraag gekregen om te kijken naar hoe kunnen we mensen met goede
@@ -18,10 +18,8 @@ export function Introduction() {
                     debriefing van het project hebben wij een context gekozen om ons onderzoek een richting te
                     geven. <br/>
                 </p>
-                <p className="mt-4 font-semibold underline">
-                    Design challenge
-                </p>
-                <p className="font-display text-4xl font-bold tracking-tight text-slate-900">
+
+                <p className="font-display text-4xl font-bold tracking-tight text-slate-900 mt-8">
                     “Hoe kunnen we mensen zonder productie-ervaring ondersteunen bij het visualiseren van een idee,
                     zodat de genre manager overtuigd is van het idee?”
                 </p>
@@ -35,6 +33,13 @@ export function Introduction() {
                     zonder productie-ervaring kunnen helpen om een idee te pitchen. Een van de uitdagingen hierbij is
                     het overtuigend overbrengen van het idee. Het liefst gaat dit visueel, omdat dit het genre manager
                     kan helpen om overtuigd te raken van het idee.
+                </p>
+
+                <p className="font-display text-4xl font-bold tracking-tight text-slate-900 mt-16">
+                    Probleemstelling
+                </p>
+                <p className="mt-4">
+                    Mensen zonder productie-ervaring hebben vaak geen idee waar zij kunnen beginnen met het visualiseren van zijn idee. In de productiewereld is een vorm hiervan storyboards, maar dit is niet de enige manier en de andere mogelijkheden willen wij onderzoeken om mensen te helpen hun idee te visualiseren.  
                 </p>
             </Container>
         </section>
